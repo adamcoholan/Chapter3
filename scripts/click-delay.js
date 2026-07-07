c3435a401fcddd6d2ce89048fc84939d97294412
Adam Coholan <46901113+adamcoholan@users.noreply.github.com>
2026-07-07 08:05:22 -0400

    add logic and clean up

--- scripts/click-delay.js
+++
@@ -1,9 +0,0 @@
-function delayAndDirect(url) {
-    setTimeout(function() {
-        setDestination(url)
-    }, 300);
-}
-
-function setDestination(url) {
-    document.location.href = url;
-}
--- scripts/loader.js
+++
@@ -1,8 +0,0 @@
-function gifLoad() {
-  setTimeout(showPage, 0);
-}
-
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("content-wrapper").style.display = "block";
-}
